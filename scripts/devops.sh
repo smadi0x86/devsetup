110ea6006db2a24d302f07743feb0cefd263b027
smadi0x86 <smadixd@gmail.com>
2025-05-19 10:06:37 +0300

    Fix typo mistake * scripts/devops.sh: C

--- scripts/devops.sh
+++ scripts/devops.sh
@@ -32,7 +32,7 @@ sudo apt-get install terraform
 
 # Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zipq
+unzip awscliv2.zip
 sudo ./aws/install
 
 # Install AZ CLI
